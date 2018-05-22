2de5d30776399384f8d8898182f68336b0e14787
martiniazzz <32032050+martiniazzz@users.noreply.github.com>
2018-05-22 22:34:25 +0300

    Rabbit's animation, death zone, rabbit's jump and background added

---
+++ Assets/DeathHere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        RabbitControl rabit = collider.GetComponent<RabbitControl>();
+        if (rabit != null)
+        {
+            LevelController.current.onRabitDeath(rabit);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/RabbitControl.cs
+++ Assets/RabbitControl.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class RabbitControl : MonoBehaviour {
 
-    public float speed = 1;
+    public float speed = 2;
     Rigidbody2D myBody = null;
+
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
 
     // Use this for initialization
     void Start () {
         myBody = this.GetComponent<Rigidbody2D>();
+        LevelController.current.setStartPosition(transform.position);
     }
 	
 	// Update is called once per frame
@@ -19,23 +26,57 @@ public class RabbitControl : MonoBehaviour {
 
     void FixedUpdate()
     {
-        //[-1, 1]
-        float value = Input.GetAxis("Horizontal");
-        if (Mathf.Abs(value) > 0)
+        Animator animator = GetComponent<Animator>();
+        float runVal = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(runVal) > 0)
         {
             Vector2 vel = myBody.velocity;
-            vel.x = value * speed;
+            vel.x = runVal * speed;
             myBody.velocity = vel;
         }
+        if (Mathf.Abs(runVal) > 0)
+            animator.SetBool("run", true);
+        else
+            animator.SetBool("run", false);
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        if (value < 0)
-        {
+        if (runVal < 0)
             sr.flipX = true;
-        }
-        else if (value > 0)
-        {
+        else if (runVal > 0)
             sr.flipX = false;
+        float diff = Time.deltaTime;
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+            isGrounded = true;
+        else
+            isGrounded = false;
+        Debug.DrawLine(from, to, Color.red);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            this.JumpActive = true;
+        if (this.JumpActive)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
         }
+        if (this.isGrounded)
+            animator.SetBool("jump", false);
+        else
+            animator.SetBool("jump", true);
     }
 
 }
